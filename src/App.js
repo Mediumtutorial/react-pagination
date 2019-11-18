b42e252f7527b520714273a7dd3e7c98fe4c36f2
Manntrix <manish.mandal21@gmail.com>
2019-11-18 18:38:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import axios from 'axios'
+import ReactPaginate from 'react-paginate';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            offset: 0,
+            data: [],
+            perPage: 10,
+            currentPage: 0
+        };
+        this.handlePageClick = this
+            .handlePageClick
+            .bind(this);
+    }
+    receivedData() {
+        axios
+            .get(`https://jsonplaceholder.typicode.com/photos`)
+            .then(res => {
+
+                const data = res.data;
+                const count = data.length
+                const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
+                const postData = slice.map(pd => <React.Fragment>
+                    <p>{pd.title}</p>
+                    <img src={pd.thumbnailUrl} alt=""/>
+                </React.Fragment>)
+
+                this.setState({
+                    pageCount: Math.ceil(data.length / this.state.perPage),
+                    count,
+                    postData
+                })
+            });
+    }
+    handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        const offset = selectedPage * this.state.perPage;
+
+        this.setState({
+            currentPage: selectedPage,
+            offset: offset
+        }, () => {
+            this.receivedData()
+        });
+
+    };
+
+    componentDidMount() {
+        this.receivedData()
+    }
+    render() {
+        return (
+            <div>
+                {this.state.postData}
+                <ReactPaginate
+                    previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={this.state.pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}/>
+            </div>
+
+        )
+    }
 }
-
-export default App;
